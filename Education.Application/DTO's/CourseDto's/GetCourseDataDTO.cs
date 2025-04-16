6080636b59f141e8476045058102a2cce97c1ce5
Hebashy12 <mohamedhebashy497@gmail.com>
2025-04-16 19:02:23 +0200

    usePatialSelectInSection&Course

---
+++ Education.Application/DTO's/CourseDto's/GetCourseDataDTO.cs
@@ -0,0 +1,25 @@
+﻿using Education.Domain.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Education.Application.DTO_s.CourseDto_s
+{
+    public class GetCourseDataDTO
+    {
+        public int CoursesId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int DiscountPercentage { get; set; } = 1;
+        public CourseStatus CourseStatus { get; set; }
+        public string CourseImage { get; set; }
+        public bool IsSequentialWatch { get; set; }
+        public int CategoriesId { get; set; }
+        public bool IsFree { get; set; }
+
+        public int? Rating { get; set; }
+    }
+}
---
+++ Education.Application/Services/CourseServices/ICourseService.cs
@@ -0,0 +1,19 @@
+﻿using Education.Application.Base;
+using Education.Application.DTO_s.CourseDto_s;
+using Education.Application.RequestModels;
+using Education.Application.ResponseModels.CourseRespondDto;
+
+namespace Education.Application.Services.CourseServices
+{
+	public interface ICourseService
+	{
+	   Task<ApiResponse<CourseRespondDto>> CreateCourse(CreateCourseDto course);
+	   Task<ApiResponse<CourseRespondDto>> GetCourseById(int CourseId);
+       Task<ApiResponse<string>> DeletedCourse(int CourseId);
+	   Task<ApiResponse<CourseRespondDto>> UpdateCourse(UpdateCourseDto course);
+	   Task<ApiResponse<string>> ChangeCourseAccess(ChangeAccessDto changeAccessDto);
+	   Task<PagedResponse<IEnumerable<CourseRespondDto>>> GetAllCources(PaginationFilter filter,string route);
+	   Task<PagedResponse<IEnumerable<CourseRespondDto>>> GetFreeCourses(PaginationFilter filter, string route);
+	   //update coure complete
+	}
+}
