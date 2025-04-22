d865b0518a6f29176a69de641464ed5f7b37e2b5
ym18112001 <ym.18112001@gmail.com>
2025-04-22 18:08:08 +0200

    Last Change For Course Service

--- Education.Application/Services/CourseServices/ICourseService.cs
+++ Education.Application/Services/CourseServices/ICourseService.cs
@@ -7,13 +7,15 @@ namespace Education.Application.Services.CourseServices
 {
 	public interface ICourseService
 	{
+
 	   Task<ApiResponse<CourseRespondDto>> CreateCourse(CreateCourseDto course);
 	   Task<ApiResponse<CourseRespondDto>> GetCourseById(int CourseId);
        Task<ApiResponse<string>> DeletedCourse(int CourseId);
 	   Task<ApiResponse<CourseRespondDto>> UpdateCourse(int courseId,UpdateCourseDto course);
 	   Task<ApiResponse<string>> ChangeCourseAccess(int courseId,ChangeAccessDto changeAccessDto);
 	   Task<PagedResponse<IEnumerable<CourseRespondDto>>> GetAllCources(PaginationFilter filter,string route);
-	   Task<PagedResponse<IEnumerable<CourseRespondDto>>> GetFreeCourses(PaginationFilter filter, string route);
+
+        Task<PagedResponse<IEnumerable<CourseRespondDto>>> GetFreeCourses(PaginationFilter filter, string route);
 	   //update coure complete
 	}
 }
